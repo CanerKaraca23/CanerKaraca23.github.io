42a94b44bd07edd90fa15b66e5fd2c453500ff2c
Caner Karaca <canerkaraca_23@hotmail.com>
2023-04-14 18:40:31 +0300

    Update

--- gtag.js
+++ gtag.js
@@ -7,3 +7,13 @@
 
   gtag('config', 'G-WNGQQLK5YG');
 </script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-56535882-4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-56535882-4');
+</script>
